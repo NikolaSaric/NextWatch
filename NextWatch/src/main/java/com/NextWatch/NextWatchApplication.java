13fd887b02cf320a26e1a21de1f4d3a22fe23212
Nikola Saric <nikolasaric531@gmail.com>
2019-07-11 19:05:31 +0200

    Initial Sping project commit.

---
+++ NextWatch/src/main/java/com/NextWatch/NextWatchApplication.java
@@ -0,0 +1,13 @@
+package com.NextWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextWatchApplication.class, args);
+	}
+
+}
