bec870abdfeb1b9360a228d2cae3e8f0edec2990
MilePrastalo <mile.prastalo97@gmail.com>
2019-07-12 10:21:48 +0200

    added router

---
+++ Front/next-watch/src/components/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-container>
+      <v-layout align-start justify-start row>Something</v-layout>
+      <v-layout align-start justify-start row>Something</v-layout>
+      <v-layout align-start justify-start row>Something</v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+}
+</script>
--- Front/next-watch/src/main.js
+++ Front/next-watch/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router.js';
+import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify'
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
