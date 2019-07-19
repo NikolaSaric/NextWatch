822548a4c14400ab80de1459823f1d764becba50
MilePrastalo <mile.prastalo97@gmail.com>
2019-07-19 17:38:50 +0200

    Small changes to front page

--- Front/next-watch/src/components/moviePreview.vue
+++ Front/next-watch/src/components/moviePreview.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img class="centered" :src="filePath"  width=200 height=300 />
+    <img  @click="viewMovie" class="centered" :src="filePath"  width=200 height=300 />
     <v-rating length=10 small  class="centered" v-model="rating"></v-rating>
     <h2 class="centered">{{movieName}}</h2>
   </div>
@@ -9,6 +9,13 @@
 <script>
 export default {
   name: 'Index',
-  props:['filePath','movieName','rating','date','year','description'],
+  props:['filePath','movieName','rating','date','year','description','id'],
+  methods:{
+    viewMovie(){
+        this.$router.push("/movie/"+this.id);
+        console.log("Clicked");
+    }
+
+}
 }
 </script>
